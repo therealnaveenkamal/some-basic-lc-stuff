0431b893278bee084a45a1710b5737e5de98d6fb
Naveenraj Kamalakannan <therealnaveenkamal@gmail.com>
2026-04-30 10:05:14 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hm = {}
+        for e in s:
+            hm[e] = hm.get(e, 0) + 1
+        
+        for e in t:
+            if e not in hm:
+                return False
+            hm[e] -=1
+
+            if hm[e] == 0:
+                del hm[e]
+        return True
+
+        
